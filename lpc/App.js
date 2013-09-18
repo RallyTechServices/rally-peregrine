b12d385f9fcd5d115fbad8418df251174051f4e3
John Martin <curmudgeon@rallydev.com>
2013-09-17 17:43:15 -0700

    move version number up

--- lpc/App.js
+++ lpc/App.js
@@ -5,7 +5,7 @@ Ext.define('CustomApp', {
 
     // Title/version
     title: 'Lean Project Charter/Releae Predictability',
-    version: '0.40',
+    version: '0.50',
 
     // Global variables
     _debug: true,
